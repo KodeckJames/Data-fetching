c065bebd6272339528dc24102c942288a629cc90
JJ <153640426+KodeckJames@users.noreply.github.com>
2026-03-08 01:53:30 +0300

    Fetched and rendered data the TanStack way

---
+++ app/tanstack/page.tsx
@@ -0,0 +1,52 @@
+'use client'
+import axios from 'axios'
+import { useQuery } from '@tanstack/react-query'
+
+interface CryptoData {
+  id: string
+  name: string
+  symbol: string
+  current_price: number
+}
+
+const api = axios.create({
+  baseURL: 'https://api.coingecko.com/api/v3',
+  headers: { 'Content-Type': 'application/json' },
+})
+
+const fetchCryptoData = async (): Promise<CryptoData[]> => {
+  const { data } = await api.get<CryptoData[]>('/coins/markets', {
+    params: {
+      vs_currency: 'usd',
+      ids: 'bitcoin',
+      names: 'Bitcoin',
+      symbols: 'btc',
+      category: 'layer-1',
+      price_change: '1h',
+    },
+  })
+  return data
+}
+
+export default function TanStackPage() {
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ['cryptoData'],
+    queryFn: fetchCryptoData,
+  })
+  if (isPending) return <div>Data Pending...</div>
+  if (isError) return <div>{error.message}</div>
+  return (
+    <div>
+      {data?.map((crypto: CryptoData) => {
+        return (
+          <div key={crypto.id} className=" gap-10 justify-center  flex flex-1 ">
+            <p>
+              {crypto.name} - {crypto.symbol}- $
+              {crypto.current_price.toLocaleString()}
+            </p>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
