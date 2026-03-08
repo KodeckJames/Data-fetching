7f097319b3b4e96815ba836aae1aaaab5956e780
JJ <153640426+KodeckJames@users.noreply.github.com>
2026-03-08 14:52:17 +0300

    Configured the React Query Devtools

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import {ReactQueryDevtools} from '@tanstack/react-query-devtools'
 
 const queryClient = new QueryClient()
 
@@ -8,6 +9,7 @@ export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
+      <ReactQueryDevtools initialIsOpen={false} buttonPosition='bottom-right' />
     </QueryClientProvider>
   )
 }
